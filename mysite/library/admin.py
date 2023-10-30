3fe3c01f2525a21aff61f149692f2aa871ec5275
Napasorn TEVARUT <napasorn.te@ku.th>
2023-10-30 23:24:16 +0700

    add initial urls and admin

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,14 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Students,Borrow,Books,Authors,Fines,Faculty,BookIssues
+
+admin.site.register(Students)
+admin.site.register(Borrow)
+admin.site.register(Books)
+admin.site.register(BookIssues)
+admin.site.register(Authors)
+admin.site.register(Fines)
+admin.site.register(Faculty)
+
+
