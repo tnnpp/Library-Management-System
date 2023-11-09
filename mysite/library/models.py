2bc5a1fd56a9dbe0588d86de9dbb2c6888b8b2c0
Napasorn <tonnam.napasorn@gmail.com>
2023-11-09 20:52:57 +0700

    add more data

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -24,7 +24,7 @@ class Users(models.Model):
 
 class Authors(models.Model):
     firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255,null=True)
 
     def __str__(self):
         return self.firstname
